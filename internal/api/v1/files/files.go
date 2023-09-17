7161c0ec6de92dcac3340fa4b6560cf13c032346
#24 <1933310737@qq.com>
2023-09-17 22:28:04 +0800

    feature: æµTest log function

--- internal/api/v1/files/files.go
+++ internal/api/v1/files/files.go
@@ -3,6 +3,7 @@ package files
 import (
 	"cld-quicker/internal/api"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"net/http"
 )
 
@@ -19,6 +20,7 @@ func (f *FileApi) SetPrefix() string {
 }
 func (f *FileApi) GetFilesList() func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
+		zap.L().Error("Some thing wrong")
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "获取文件列表",
 		})
