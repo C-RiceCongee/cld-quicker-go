f5226719361d4a86abe92f1d4764aec5d63b009b
#24 <1933310737@qq.com>
2023-09-17 20:58:21 +0800

    first init çLOL ！！

---
+++ cmd/gen/gen.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"cld-quicker/database/mysql"
+	"cld-quicker/pkg"
+	"fmt"
+	"gorm.io/gen"
+	"gorm.io/gorm"
+	"log"
+)
+
+func Gen(db *gorm.DB) {
+	g := gen.NewGenerator(gen.Config{
+		OutPath:      "./internal/model/dal",
+		ModelPkgPath: "./internal/model/models",
+		OutFile:      "",
+		Mode:         gen.WithoutContext | gen.WithDefaultQuery,
+	})
+	g.UseDB(db)
+	g.ApplyBasic(g.GenerateAllTable()...)
+	g.Execute()
+}
+func main() {
+	// 初始化配置
+	pkg.InitConfig()
+
+	// 链接数据库拿到DB
+	db, err := mysql.InitSqlxDB()
+	fmt.Print(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+	Gen(db)
+}
